eb86aa0ece5c59218f6aea8f2fa716b1ab9b8170
jynam <njy1007@gmail.com>
2023-01-10 22:11:07 +0900

    chore: 어비 은경 폰트 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 
 import Head from 'next/head';
+import '../public/fonts/index.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
