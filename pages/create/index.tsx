73b2087a146dec645e07f724f37a566746b4d9b1
Sunghum Paik <zerosial@gmail.com>
2023-01-11 18:43:52 +0900

    feat: 1차 마크업 완료

--- pages/create/index.tsx
+++ pages/create/index.tsx
@@ -1,8 +1,6 @@
 import LogoSvg from '@public/svg/logo.svg';
 import styled from '@emotion/styled';
 
-const FormList = styled.div``;
-
 const Main = styled.div`
   width: 80%;
   height: 60%;
@@ -12,7 +10,9 @@ const Main = styled.div`
   display: flex;
 `;
 
-const LogoSide = styled.div`
+const Side = styled.div`
+  margin-left: 100px;
+  margin-top: 150px;
   width: 500px;
   height: 600px;
   display: flex;
@@ -22,33 +22,35 @@ const LogoSide = styled.div`
 
 const Logo = styled.div`
   width: 400px;
-  height: 300px;
+  height: 200px;
   display: flex;
   align-items: center;
-  gap: 20px;
+  gap: 50px;
 `;
 
-const Form = styled.form`
-  width: 600px;
-  height: 600px;
-  background-color: white;
-  display: flex;
-  flex-direction: column;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 20px;
-  padding: 40px;
-`;
-
-const Title = styled.h3`
+const LogoTitle = styled.h3`
   font-family: 'UhBee EUN KYUNG';
   font-size: 80px;
   font-weight: 700;
 `;
 
-const SubTitle = styled.div`
+const LogoSubTitle = styled.div`
   font-family: 'UhBee EUN KYUNG';
   font-size: 48px;
   font-weight: 400;
+`;
+
+const Form = styled.form`
+  margin-left: 50px;
+  width: 500px;
+  height: 700px;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+  padding: 0 45px 55px 55px;
+  gap: 30px;
 `;
 
 const FormTitle = styled.h3`
@@ -61,20 +63,43 @@ const FormSubTitle = styled.label`
   font-size: 20px;
 `;
 
-const FormInput = styled.input``;
+const FormList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
 
-const FormButton = styled.button``;
+const FormInput = styled.input`
+  width: 480px;
+  height: 60px;
+  border: 2px solid #b3b3b3;
+  border-radius: 5px;
+`;
+const FormButton = styled.button`
+  width: 480px;
+  height: 80px;
+  margin-top: 20px;
+  background-color: #d44645;
+  border-radius: 20px;
+  border-color: white;
+  font-family: 'UhBee EUN KYUNG';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 32px;
+  text-align: center;
+  color: white;
+`;
 
 export default function Create() {
   return (
     <Main>
-      <LogoSide>
+      <Side>
         <Logo>
           <LogoSvg />
-          <Title>뽀모</Title>
+          <LogoTitle>뽀모</LogoTitle>
         </Logo>
-        <SubTitle>같이 뽀모해요</SubTitle>
-      </LogoSide>
+        <LogoSubTitle>같이 뽀모해요</LogoSubTitle>
+      </Side>
       <Form>
         <FormTitle>회원가입</FormTitle>
         <FormList>
@@ -84,7 +109,6 @@ export default function Create() {
         <FormList>
           <FormSubTitle>비밀번호</FormSubTitle>
           <FormInput type="password" placeholder="비밀번호를 입력해 주세요" />
-          <FormSubTitle>비밀번호 재입력</FormSubTitle>
           <FormInput type="password" placeholder="비밀번호를 다시 입력해 주세요" />
         </FormList>
         <FormList>
