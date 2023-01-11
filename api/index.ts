0dd2ae7a1410546d8d3ea5898fa56f60e940c2ec
jynam <njy1007@gmail.com>
2023-01-11 23:43:58 +0900

    chore: axiosInstance 만들기

---
+++ api/index.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    Accept: '*/*',
+    'Content-Type': 'application/json',
+  },
+});
