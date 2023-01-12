833388add4a105cf6734a01509a204a38656e6e4
EUNA <beni1026@naver.com>
2023-01-12 23:55:46 +0900

    fix: 페이지 주소 / 로 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,20 @@
+import CategoryCard from '@components/home/CategoryCard';
+
 export default function Home() {
-  return <></>;
+  return (
+    <main>
+      <div>
+        <div>같이 뽀모해요.</div>
+        <input type="text" />
+        <div>
+          카테고리목록
+          <CategoryCard />
+        </div>
+      </div>
+      <div>
+        <input type="text" />
+        <ul>유저 목록</ul>
+      </div>
+    </main>
+  );
 }
