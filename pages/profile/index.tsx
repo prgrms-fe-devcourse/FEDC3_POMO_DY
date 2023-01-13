5620adecdf80f49aad21d2fb2a4e0d8c39095d87
Sunghum Paik <zerosial@gmail.com>
2023-01-13 18:44:10 +0900

    feat: 임시 Nav바 설정

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,7 +1,34 @@
+import styled from '@emotion/styled';
+import { COLORS } from 'styles/colors';
+
+const Main = styled.div`
+  margin-top: 100px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+`;
+
+const Nav = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 96px;
+  background: ${COLORS.sub_yellow};
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid black;
+`;
+
 export default function MyProfile() {
   return (
     <>
-      <div>마이 프로필</div>
+      <Nav>Nav 임시</Nav>
+      <Main>
+        <div>마이 프로필</div>
+      </Main>
     </>
   );
 }
