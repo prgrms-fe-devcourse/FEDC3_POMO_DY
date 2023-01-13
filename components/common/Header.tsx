62131ca87d149e5ffda26fc1e8626a7cd0047ad7
KSoonYo <developsoon@gmail.com>
2023-01-14 00:49:16 +0900

    feat: 네비게이션 바 마크업

---
+++ components/common/Header.tsx
@@ -0,0 +1,80 @@
+import styled from '@emotion/styled';
+import { FunctionComponent } from 'react';
+import LogoSmall from '@public/images/logo-small.svg';
+import Search from '@components/common/Search';
+import { css } from '@emotion/react';
+
+// const dummy = {
+//   data: [],
+// };
+
+const Bar = styled.nav`
+  display: flex;
+  align-items: center;
+  height: 100px;
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  min-width: 120px;
+  align-items: center;
+  margin-left: 16px;
+`;
+const LogoTitle = styled.div`
+  font-family: 'UhBee EUN KYUNG';
+  font-size: 1.5rem;
+  font-weight: 700;
+`;
+
+const Spacer = styled.div`
+  flex-grow: 1;
+`;
+
+const User = styled.div`
+  display: flex;
+  height: 100%;
+  margin: 0px 20px;
+  align-items: center;
+  gap: 15px;
+`;
+
+const imageCircle = css`
+  width: 35px;
+  height: 35px;
+  border: 1px solid transparent;
+  border-radius: 30px;
+  cursor: pointer;
+`;
+
+const imageUrl = (props: { src?: string }) => `
+background-image: ${props.src ? `url(${props.src});` : `url(/images/logo-small.svg);`}
+background-repeat: no-repeat;
+background-size: 100% 100%;  
+`;
+
+const Profile = styled.div`
+  ${imageCircle};
+  ${imageUrl}
+`;
+
+const Notification = styled.div`
+  ${imageCircle};
+  ${imageUrl}
+`;
+
+export const Header: FunctionComponent = () => {
+  return (
+    <Bar>
+      <LogoContainer>
+        <LogoSmall style={{ marginRight: '16px' }} />
+        <LogoTitle> 뽀모 </LogoTitle>
+      </LogoContainer>
+      <Spacer />
+      <Search />
+      <User>
+        <Profile />
+        <Notification src="/images/notification.svg" />
+      </User>
+    </Bar>
+  );
+};
