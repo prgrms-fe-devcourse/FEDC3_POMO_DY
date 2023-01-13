9ba63575b5274aacff26da2e5a07ee4b07aa2f55
Sunghum Paik <zerosial@gmail.com>
2023-01-14 02:58:51 +0900

    feat: 1차 마크업 (유저 정보)

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,10 +1,10 @@
 import styled from '@emotion/styled';
 import { COLORS } from 'styles/colors';
-
+import BigProfileSvg from '@public/images/bigprofile.svg';
+import PencilSvg from '@public/icons/pencil.svg';
+import ProfileSvg from '@public/icons/profile.svg';
 const Main = styled.div`
   margin-top: 100px;
-  justify-content: center;
-  align-items: center;
   display: flex;
 `;
 
@@ -22,12 +22,75 @@ const Nav = styled.div`
   border: 2px solid black;
 `;
 
+const Info = styled.div`
+  display: flex;
+  gap: 42px;
+  margin-top: 92px;
+  margin-left: 130px;
+`;
+
+const InfoDetail = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  height: 50px;
+  justify-content: center;
+  align-items: center;
+  margin-top: 33px;
+`;
+
+const InfoUserName = styled.div`
+  height: 48px;
+  font-weight: 800;
+  font-size: 40px;
+  line-height: 48px;
+`;
+
+const InfoUserEmail = styled.div`
+  height: 24px;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 24px;
+  grid-column: span 2;
+`;
+
+const PasswordChangeButton = styled.button`
+  margin-left: 676px;
+  margin-top: 138px;
+  width: 162px;
+  height: 48px;
+  background: ${COLORS.main};
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 21px;
+  text-align: center;
+  color: white;
+  border: none;
+  border-radius: 28px;
+  border-color: ${COLORS.main};
+`;
+
 export default function MyProfile() {
   return (
     <>
       <Nav>Nav 임시</Nav>
       <Main>
-        <div>마이 프로필</div>
+        <Info>
+          <BigProfileSvg />
+          <InfoDetail>
+            <InfoUserName>김뽀모</InfoUserName>
+            <PencilSvg />
+            <InfoUserEmail>pomo@gmail.com</InfoUserEmail>
+          </InfoDetail>
+        </Info>
+        <PasswordChangeButton>비밀번호 변경</PasswordChangeButton>
+        <div>
+          <div>팔로워</div>
+          <div>팔로잉</div>
+        </div>
+        <div>
+          <ProfileSvg />
+          <div>김규란</div>
+        </div>
       </Main>
     </>
   );
