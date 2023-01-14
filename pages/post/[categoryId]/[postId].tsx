5df1ae2a01caeabdc980df2ad30dc4bca4bbe759
jynam <njy1007@gmail.com>
2023-01-15 00:44:33 +0900

    feat: 글 정보 영역 마크업

--- pages/post/[categoryId]/[postId].tsx
+++ pages/post/[categoryId]/[postId].tsx
@@ -1,8 +1,10 @@
+import PostInfo from '@components/post/info';
 import PostTabs from '@components/post/tabs';
 
 export default function PostPage() {
   return (
     <div>
+      <PostInfo />
       <PostTabs />
     </div>
   );
