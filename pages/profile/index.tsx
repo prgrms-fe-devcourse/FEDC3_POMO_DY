365df4d7ea6fcd1e78e338ec7e1c4619ca1986d0
Sunghum Paik <zerosial@gmail.com>
2023-01-14 19:43:52 +0900

    feat: 디자인 1차 완료

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,11 +1,10 @@
 import styled from '@emotion/styled';
 import { COLORS } from 'styles/colors';
-import BigProfileSvg from '@public/images/bigprofile.svg';
 import PencilSvg from '@public/icons/pencil.svg';
 import ProfileSvg from '@public/icons/profile.svg';
 
 const Main = styled.div`
-  margin-top: 100px;
+  margin-top: 90px;
   height: 100vh;
   background-color: white;
   display: grid;
@@ -32,6 +31,11 @@ const Info = styled.div`
   gap: 42px;
   margin-top: 92px;
   margin-left: 130px;
+  & > .infoIcon {
+    display: block;
+    width: 160px;
+    height: 160px;
+  }
 `;
 
 const InfoDetail = styled.div`
@@ -41,6 +45,7 @@ const InfoDetail = styled.div`
   justify-content: center;
   align-items: center;
   margin-top: 33px;
+  gap: 10px;
 `;
 
 const InfoUserName = styled.div`
@@ -76,6 +81,31 @@ const PasswordChangeButton = styled.button`
 
 const Like = styled.div`
   display: grid;
+  margin-left: 130px;
+  margin-top: 125px;
+  gap: 25px;
+`;
+
+const LikeTap = styled.div`
+  display: flex;
+  gap: 28px;
+  margin-bottom: 40px;
+`;
+const LikeTapItem = styled.div`
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 24px;
+  color: #000000;
+`;
+const User = styled.div`
+  display: flex;
+`;
+const UserName = styled.div`
+  margin-left: 20px;
+  margin-top: 10px;
+  font-weight: 600;
+  font-size: 25px;
+  line-height: 30px;
 `;
 
 export default function MyProfile() {
@@ -84,7 +114,7 @@ export default function MyProfile() {
       <Nav>Nav 임시</Nav>
       <Main>
         <Info>
-          <BigProfileSvg />
+          <ProfileSvg className="infoIcon" />
           <InfoDetail>
             <InfoUserName>김뽀모</InfoUserName>
             <PencilSvg />
@@ -93,14 +123,22 @@ export default function MyProfile() {
         </Info>
         <PasswordChangeButton>비밀번호 변경</PasswordChangeButton>
         <Like>
-          <div>
-            <div>팔로워</div>
-            <div>팔로잉</div>
-          </div>
-          <div>
+          <LikeTap>
+            <LikeTapItem>팔로워 2명</LikeTapItem>
+            <LikeTapItem>팔로잉 10명</LikeTapItem>
+          </LikeTap>
+          <User>
             <ProfileSvg />
-            <div>김규란</div>
-          </div>
+            <UserName>김규란</UserName>
+          </User>
+          <User>
+            <ProfileSvg />
+            <UserName>내이름은슈퍼초길지</UserName>
+          </User>
+          <User>
+            <ProfileSvg />
+            <UserName>라면국물</UserName>
+          </User>
         </Like>
       </Main>
     </>
