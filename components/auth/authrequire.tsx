6a9c5a2646692dd9a2f27f582c5f44f722e3766d
Sunghum Paik <zerosial@gmail.com>
2023-01-15 10:17:15 +0900

    feat: typescript 추가

--- components/auth/authrequire.tsx
+++ components/auth/authrequire.tsx
@@ -1,8 +1,13 @@
 import { useRouter } from 'next/router';
 import { getLocalstorage } from '@components/auth/localstorage';
 import { useOnce } from 'hooks/useOnce';
+import { FC, ReactNode } from 'react';
 
-export const AuthRequired = ({ children }) => {
+interface AuthRequiredProps {
+  children: ReactNode;
+}
+
+export const AuthRequired: FC<AuthRequiredProps> = ({ children }) => {
   const router = useRouter();
   useOnce(() => {
     if (getLocalstorage('JWT_TOKEN') === null) {
