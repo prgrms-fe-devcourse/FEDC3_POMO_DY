aec833db327f4586136976c41785293b9e00f09c
jynam <njy1007@gmail.com>
2023-01-15 23:40:07 +0900

    fix: api 요청 방식 next api 활용하도록 변경

--- components/post/hooks/queries.ts
+++ components/post/hooks/queries.ts
@@ -1,12 +1,13 @@
-import { getPost } from 'api/post';
+import { axiosInstance } from 'api';
+import { AxiosResponse } from 'axios';
 import { useQuery } from 'react-query';
 import { Post } from '../types';
 
 export const useGetPost = (id: string) =>
-  useQuery<string, unknown, Post, string[]>(
+  useQuery<AxiosResponse<Post, unknown>, unknown, Post, string[]>(
     ['post_getPost', id],
     async () => {
-      const data = await getPost(id);
+      const { data } = await axiosInstance.get(`/api/posts/${id}`);
       return data;
     },
     { enabled: !!id },
