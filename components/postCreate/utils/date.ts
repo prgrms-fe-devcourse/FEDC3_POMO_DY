42f2b3dfb5a0ff8ac1d2d295cd82281bcbdba494
gxxrxn <rbfks1221@naver.com>
2023-01-17 18:18:27 +0900

    fix: caclTodayDateKST undefined 예외 처리

--- components/postCreate/utils/date.ts
+++ components/postCreate/utils/date.ts
@@ -1,9 +1,8 @@
 export const calcTodayDateKST = () => {
-  const [year, month, date]: string[] = new Date()
-    .toLocaleDateString()
-    .split('.')
-    .slice(0, -1)
-    .map((v) => v.trim());
+  const todayDate = new Date().toLocaleDateString().split('.');
 
+  if (!todayDate) return '';
+
+  const [year, month, date]: string[] = todayDate.slice(0, -1).map((v) => v.trim());
   return `${year}-${month.padStart(2, '0')}-${date.padStart(2, '0')}`;
 };
