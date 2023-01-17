fc33c7fcda5e7a075614d96b057df3f4c4388634
gxxrxn <rbfks1221@naver.com>
2023-01-17 18:26:14 +0900

    feat: calcTodayDateKST year, month, date 변수 예외처리

--- components/postCreate/utils/date.ts
+++ components/postCreate/utils/date.ts
@@ -1,8 +1,9 @@
 export const calcTodayDateKST = () => {
   const todayDate = new Date().toLocaleDateString().split('.');
-
   if (!todayDate) return '';
 
   const [year, month, date]: string[] = todayDate.slice(0, -1).map((v) => v.trim());
+  if (!year || !month || !date) return '';
+
   return `${year}-${month.padStart(2, '0')}-${date.padStart(2, '0')}`;
 };
