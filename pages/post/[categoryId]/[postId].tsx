64f1abcf123df74f76c881a234380216d73170a6
jynam <njy1007@gmail.com>
2023-01-18 15:12:58 +0900

    fix: production에서 동적 라우팅 api 404 에러 fix

--- pages/post/[categoryId]/[postId].tsx
+++ pages/post/[categoryId]/[postId].tsx
@@ -1,12 +1,13 @@
 import { useGetPost } from '@components/post/hooks/queries';
 import PostInfo from '@components/post/info';
 import PostTabs from '@components/post/tabs';
-// import { Post } from '@components/post/types';
-// import { GetServerSideProps } from 'next';
+import { Post } from '@components/post/types';
+import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
-// import { ParsedUrlQuery } from 'querystring';
+import { ParsedUrlQuery } from 'querystring';
 
 export default function PostPage() {
+  window && console.log(window.location.href);
   const router = useRouter();
   const { postId } = router.query;
   const { data } = useGetPost(postId as string);
@@ -18,26 +19,26 @@ export default function PostPage() {
   );
 }
 
-// interface Params extends ParsedUrlQuery {
-//   postId: string;
-// }
+interface Params extends ParsedUrlQuery {
+  postId: string;
+}
 
-// const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   const { postId } = context.params as Params;
-//   const post = await fetch(
-//     `${isProduction ? 'https://fedc-3-pomo-dy.vercel.app' : 'http://localhost:3000'}/api/posts/${postId}`,
-//   );
-//   const data = await post.json();
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { postId } = context.params as Params;
+  const post = await fetch(
+    `${isProduction ? 'https://fedc-3-pomo-dy.vercel.app' : 'http://localhost:3000'}/api/posts/${postId}`,
+  );
+  const data = await post.json();
 
-//   if (!data) {
-//     return {
-//       notFound: true,
-//     };
-//   }
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
 
-//   return {
-//     props: { post: data as Post },
-//   };
-// };
+  return {
+    props: { post: data as Post },
+  };
+};
