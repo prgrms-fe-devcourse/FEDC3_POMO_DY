920b4de187eb8d193e2e93e9ff444c9d3a0df312
jynam <njy1007@gmail.com>
2023-01-18 14:40:11 +0900

    fix: production에서 동적 라우팅 api 404 에러 fix

--- pages/post/[categoryId]/[postId].tsx
+++ pages/post/[categoryId]/[postId].tsx
@@ -1,7 +1,10 @@
 import { useGetPost } from '@components/post/hooks/queries';
 import PostInfo from '@components/post/info';
 import PostTabs from '@components/post/tabs';
+import { Post } from '@components/post/types';
+import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
+import { ParsedUrlQuery } from 'querystring';
 
 export default function PostPage() {
   const router = useRouter();
@@ -14,3 +17,27 @@ export default function PostPage() {
     </div>
   );
 }
+
+interface Params extends ParsedUrlQuery {
+  postId: string;
+}
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { postId } = context.params as Params;
+  const post = await fetch(
+    `${isProduction ? 'https://fedc-3-pomo-dy.vercel.app' : 'http://localhost:3000'}/api/posts/${postId}`,
+  );
+  const data = await post.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { post: data as Post },
+  };
+};
