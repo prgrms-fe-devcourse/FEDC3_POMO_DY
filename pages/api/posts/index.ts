dca9bde8d8d41e2ccadc031a926cde84e63aff79
jynam <njy1007@gmail.com>
2023-01-18 14:51:59 +0900

    fix: production에서 동적 라우팅 api 404 에러 fix

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -1,9 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function posts(_request: NextApiRequest, response: NextApiResponse) {
-  try {
-    return response.status(200).end();
-  } catch (err) {
-    return response.status(500).end(JSON.stringify(err));
-  }
+  return response.status(200).end();
 }
