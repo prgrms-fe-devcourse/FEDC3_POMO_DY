5918f645bbb4834d9ebce8c917e55a6e5ac94734
KSoonYo <developsoon@gmail.com>
2023-01-20 22:36:49 +0900

    fix: 404페이지 네비게이션 바 제거

--- components/layout/BaseLayout.tsx
+++ components/layout/BaseLayout.tsx
@@ -6,7 +6,7 @@ export default function BaseLayout(props: { children: ReactNode }) {
   const router = useRouter();
 
   const isNotAccessable = useMemo(
-    () => router.pathname.startsWith('/login') || router.pathname.startsWith('/sign'),
+    () => router.pathname.startsWith('/login') || router.pathname.startsWith('/sign') || router.route === '/404',
     [router.pathname],
   );
 
