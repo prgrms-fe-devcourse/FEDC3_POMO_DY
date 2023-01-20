1c800db2c53464b723291c102b9c9b83eaf6c7f6
jynam <njy1007@gmail.com>
2023-01-20 21:55:22 +0900

    feat: 가려진 댓글 마크업

--- components/post/tabs/comment/CommentItem.tsx
+++ components/post/tabs/comment/CommentItem.tsx
@@ -1,12 +1,23 @@
 import styled from '@emotion/styled';
 import EmptyProfileImage from 'public/icons/profile.svg';
+import { COLORS } from 'styles/colors';
+import SadPomo from '@public/images/logo-sad.svg';
 
 interface CommentItemProps {
   comment: string;
+  isHidden?: boolean;
 }
 
-export default function CommentItem({ comment }: CommentItemProps) {
-  return (
+export default function CommentItem({ comment, isHidden }: CommentItemProps) {
+  return isHidden ? (
+    <HiddenComment>
+      <Circle />
+      <HiddenText>
+        휴식 시간에 볼 수 있어요
+        <SadPomo className="sad-pomo" />
+      </HiddenText>
+    </HiddenComment>
+  ) : (
     <Container>
       <EmptyProfileImage />
       <Comment>{comment}</Comment>
@@ -31,4 +42,37 @@ const Comment = styled.div`
   color: #000000;
   display: flex;
   align-items: center;
+`;
+
+const HiddenComment = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  .sad-pomo {
+    width: 27px;
+    height: 21px;
+  }
+`;
+
+const HiddenText = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 6px;
+  width: calc(100% - 73px);
+  background-color: ${COLORS.sub_green};
+  border-radius: 20px;
+  padding: 16px 20px;
+  color: white;
+  font-family: 'UhBee EUN KYUNG';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 30px;
+`;
+
+const Circle = styled.div`
+  border-radius: 50%;
+  width: 56px;
+  height: 56px;
+  background-color: #d0e0cb;
 `;
