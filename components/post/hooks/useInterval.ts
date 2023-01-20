4f550110198adabdb4e8ee522e5d0e3eb413170f
jynam <njy1007@gmail.com>
2023-01-20 14:47:35 +0900

    chore: setInterval 수동으로 시작할 수 있도록

--- components/post/hooks/useInterval.ts
+++ components/post/hooks/useInterval.ts
@@ -2,23 +2,31 @@ import { useEffect, useRef } from 'react';
 
 type IntervalId = ReturnType<typeof setInterval>;
 
-export default function useInterval(callback: () => void, interval: number) {
+export default function useInterval(callback: () => void, interval: number, isAutoStart = true) {
   const intervalId = useRef<IntervalId | null>(null);
 
   const cancel = (id: IntervalId) => {
     clearInterval(id);
   };
 
-  useEffect(() => {
+  const start = () => {
+    if (isAutoStart) return;
     intervalId.current = setInterval(callback, interval);
+  };
+
+  useEffect(() => {
+    if (isAutoStart) {
+      intervalId.current = setInterval(callback, interval);
+    }
     return () => {
       if (intervalId.current !== null) {
         cancel(intervalId.current);
       }
     };
-  }, [callback, interval]);
+  }, [callback, interval, isAutoStart]);
 
   return {
     cancel,
+    start,
   };
 }
